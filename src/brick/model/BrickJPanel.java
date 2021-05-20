34bbb31b273b81b240856672a097c9c7c489d54c
ArmaLite <70229957+sturmtruppe@users.noreply.github.com>
2021-05-19 19:26:44 -0600

    imports for Panel, added all stubs,  game start, ball starting pos and slider pos :D

--- src/brick/model/BrickJPanel.java
+++ src/brick/model/BrickJPanel.java
@@ -1,5 +1,65 @@
 package brick.model;
 
-public class BrickJPanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
+import javax.swing.JPanel;
+
+public class BrickJPanel extends JPanel implements KeyListener, ActionListener
+{
+	// Game start(won't let game play by itself)
+	private boolean play = false;
+	private int score = 0;
+	
+	private int totalBricks = 21;
+	
+	//Ball speed
+	private Timer time;
+	private int delay = 8;
+	
+	
+	private int playerX = 310;
+	
+	private int ballposX = 120;
+	private int ballposY = 350;
+	private int ballXdir = -1;
+	private int ballYdir = -2;
+	
+	public ()
+	{
+		addKeyListener
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 }
